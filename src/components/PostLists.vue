fc029309b5b3593bcdfd5b50e92f44eec2ba553e
Aloxan12 <kvakvaaak@mail.ru>
2021-08-08 11:42:08 +0300

    refactoring

---
+++ src/components/PostLists.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>Список пользователей</h3>
+    <post-item :post="post" v-for="post in posts"  v-bind:key="post" />
+  </div>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+export default {
+  components: {PostItem},
+  props:{
+  posts:{
+    type: Array,
+    required: true
+  }
+}
+}
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
