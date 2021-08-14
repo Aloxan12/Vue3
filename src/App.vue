6a54069f3b8056e704d8cb15b4093965e2b0771c
Aloxan12 <kvakvaaak@mail.ru>
2021-08-14 12:53:40 +0300

    router

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,16 @@
 <template>
+  <navbar></navbar>
   <div class="app">
-    <h1>Страница с постами</h1>
-    <my-input
-      v-model="searchQuery"
-      placeholder="Поиск..."
-    />
-    <div class="app_btns">
-      <my-button
-          @click="showDialog"
-      >
-        Создать пост
-      </my-button>
-      <my-select
-          v-model="selectedSort"
-          :options="sortOptions"
-      />
-    </div>
-    <my-dialog v-model:show="dialogVisible">
-      <post-form @create="createPost"/>
-    </my-dialog>
-    <post-lists
-        :posts="sortedAndSearchedPosts"
-        @remove="removePost"
-        v-if="!isPostsLoading"
-    />
-    <div v-else>Loading...</div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import PostForm from "@/components/PostForm";
-import PostLists from "@/components/PostLists";
-import MyDialog from "@/components/UI/MyDialog";
-import MyButton from "@/components/UI/MyButton";
-import axios from "axios";
-import MySelect from "@/components/UI/MySelect";
-import MyInput from "@/components/UI/MyInput";
-
+import Navbar from '@/components/UI/Navbar'
 export default {
-  components: {
-    MyInput,
-    MySelect,
-    MyButton,
-    MyDialog,
-    PostForm, PostLists
-  },
-  data() {
-    return {
-      posts: [],
-      dialogVisible: false,
-      isPostsLoading: false,
-      selectedSort: '',
-      searchQuery: '',
-      page:1,
-      limit: 10,
-      totalPage: 0,
-      sortOptions: [
-        {value: 'title', name: 'По названию'},
-        {value: 'body', name: 'По содержанию'},
-      ]
-    }
-  },
-  methods: {
-    createPost(post) {
-      this.posts.push(post)
-      this.dialogVisible = false
-    },
-    removePost(post) {
-      this.posts = this.posts.filter(p => p.id !== post.id)
-    },
-    showDialog() {
-      this.dialogVisible = true;
-    },
-    async fetchPosts() {
-      try {
-        this.isPostsLoading = true;
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?',{
-          params:{
-            _page: this.page,
-            limit: this.limit
-          }
-        })
-        this.totalPage = Math.ceil(response.headers['x-total-count']/ this.limit)
-        this.posts = response.data;
-      } catch (e) {
-        alert('Error', e)
-      } finally {
-        this.isPostsLoading = false
-      }
-    },
-  },
-  mounted() {
-    this.fetchPosts()
-  },
-  computed: {
-    sortedPosts() {
-      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
-    },
-    sortedAndSearchedPosts(){
-      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
-    }
-  },
-  watch: {}
+components:{
+  Navbar
+}
 }
 </script>
 
@@ -114,11 +23,5 @@ export default {
 
 .app {
   padding: 20px;
-}
-
-.app_btns {
-  margin: 15px 0;
-  display: flex;
-  justify-content: space-between;
 }
 </style>
---
+++ src/components/UI/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')">Vue 3</div>
+    <div class="navbar_btns" >
+      <my-button @click="$router.push('/posts')">Пользователи</my-button>
+      <my-button
+          @click="$router.push('/about')"
+          style="margin-left: 20px">О приложении</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyButton from "@/components/UI/MyButton";
+export default {
+  components: {MyButton}
+}
+</script>
+
+<style scoped>
+.navbar{
+  height: 50px;
+  background: lightgrey;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+.navbar_btns{
+  margin-left: auto;
+}
+</style>
