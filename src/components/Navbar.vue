329e634cfc52ea7840d68659fd1cb070ba41ab39
Aloxan12 <kvakvaaak@mail.ru>
2021-08-14 14:06:55 +0300

    directives

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <div @click="$router.push('/')">Vue 3</div>
     <div class="navbar_btns" >
-      <my-button @click="$router.push('/posts')">Пользователи</my-button>
+      <my-button @click="$router.push('/posts')">Посты</my-button>
       <my-button
           @click="$router.push('/about')"
           style="margin-left: 20px">О приложении</my-button>
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Страница с постами</h1>
     <my-input
+        v-focus
         v-model="searchQuery"
         placeholder="Поиск..."
     />
@@ -25,7 +26,7 @@
         v-if="!isPostsLoading"
     />
     <div v-else>Loading...</div>
-    <div ref="observer" class="observer"></div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
     <!--    <div class="page_wrapper">-->
     <!--      <div-->
     <!--          v-for="pageNumber in totalPage"-->
@@ -123,18 +124,18 @@ export default {
   },
   mounted() {
     this.fetchPosts();
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0
-    }
-    const callback = (entries, observer) => {
-      console.log(observer)
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts()
-      }
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer);
+    // const options = {
+    //   rootMargin: '0px',
+    //   threshold: 1.0
+    // }
+    // const callback = (entries, observer) => {
+    //   console.log(observer)
+    //   if (entries[0].isIntersecting && this.page < this.totalPages) {
+    //     this.loadMorePosts()
+    //   }
+    // };
+    // const observer = new IntersectionObserver(callback, options);
+    // observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
